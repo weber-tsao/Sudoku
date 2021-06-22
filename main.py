b3a19f8e81df0a659444e0a8917e8aa5ab921e00
weber-tsao <67998511+weber-tsao@users.noreply.github.com>
2021-06-22 22:57:32 +0100

    add function to change board and see solution button

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from sudoku import *
 
 app = Flask(__name__)
 
-# Defining the home page of our site
-@app.route("/")
-def home():
-    ####TODO delete x and make it empty first and appear number when click button 
-    return render_template("index.html", x = create_puzzle(sudoku_generator()))
+random_board = create_puzzle(sudoku_generator())
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-@app.route("/admin")
-def admin():
-	return redirect(url_for("home"))
+# Defining the home page of the web app
+####TODO delete x and make it empty first and appear number when click button 
+# (or use session to store the previous board)####
+@app.route("/sudoku", methods=["POST", "GET"])
+def sudoku():
+    
+    if request.method == "POST":
+        if request.form.get("change_board"):
+            global random_board 
+            random_board = create_puzzle(sudoku_generator())
+            return render_template("index.html", x = random_board)
+        
+        elif request.form.get("check_answer"):
+            return render_template("index.html", x = random_board)
+        
+        elif request.form.get("see_solution"):
+            solve_sudoku(random_board)
+            return render_template("index.html", x = random_board)
+        
+        else: 
+            return render_template("index.html", x = random_board)
+        
+    else:
+        return render_template("index.html", x = random_board)
 
 if __name__ == "__main__":
     app.run()
